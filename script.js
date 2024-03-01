2b33f7fde0ad8798c468c63f87297601d4ad4cba
Nipa <moninipa027@gmail.com>
2024-03-01 10:36:47 +0600

    about section done

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
    function changebg() {
     var navber =document.getElementById('navbg');
     var scrl_value =window.scrollY;
-    if (scrollY < 150) {
+    if (scrollY < 200) {
         navber.classList.remove('nav-change');
         
     }else{
